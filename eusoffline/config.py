5bdf35c3ec05dec252f168baf0bde1977b1a5ece
isbobby <bobbyclex8@gmail.com>
2020-03-14 15:07:05 +0800

    added attached database URI

--- eusoffline/config.py
+++ eusoffline/config.py
@@ -1,4 +1,6 @@
 class Config:
     SECRET_KEY = 'c9970460fc2c3ad324add53c94e3bc2a'
     USER_APP_NAME = "Eusoffline"
-    
+    SQLALCHEMY_DATABASE_URI = "postgres://ittldgsyeuwklc:e8222e6169a4f74b4725bd21cfdb566177a4591ab52eedee4603c9fec7a07e4b@ec2-52-73-247-67.compute-1.amazonaws.com:5432/d8il6ph5hhlh28"
+
+
